f605008b7da6018f50cdf9bc8dac909cbf617133
Alexey Koposov <cmango67@gmail.com>
2022-12-17 12:30:14 +0500

    Add di

---
+++ WeatherBot/WeatherBot/DI/BotContainerBuilder.cs
@@ -0,0 +1,13 @@
+﻿using Autofac;
+
+namespace WeatherBot.DI;
+
+public static class BotContainerBuilder
+{
+    public static IContainer Build()
+    {
+        var containerBuilder = new ContainerBuilder();
+
+        return containerBuilder.Build();
+    }
+}
