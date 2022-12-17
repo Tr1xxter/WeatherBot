5a8a5f22e24a3b0e59b20960afe9d97e4fb61bb5
Alexey Koposov <cmango67@gmail.com>
2022-12-17 12:44:30 +0500

    Add logging

--- WeatherBot/WeatherBot/DI/BotContainerBuilder.cs
+++ WeatherBot/WeatherBot/DI/BotContainerBuilder.cs
@@ -1,4 +1,8 @@
 ﻿using Autofac;
+using Vostok.Logging.Abstractions;
+using Vostok.Logging.Console;
+using Vostok.Logging.File;
+using Vostok.Logging.File.Configuration;
 
 namespace WeatherBot.DI;
 
@@ -7,6 +11,23 @@ public static class BotContainerBuilder
     public static IContainer Build()
     {
         var containerBuilder = new ContainerBuilder();
+
+        containerBuilder.Register<ILog>(cc =>
+        {
+            var consoleLog = new ConsoleLog();
+            var fileLogSettings = new FileLogSettings
+            {
+                RollingStrategy = new RollingStrategyOptions
+                {
+                    MaxFiles = 10,
+                    MaxSize = 1024 * 1024 * 100,
+                    Period = RollingPeriod.Day,
+                    Type = RollingStrategyType.Hybrid
+                }
+            };
+            var fileLog = new FileLog(fileLogSettings);
+            return new CompositeLog(consoleLog, fileLog);
+        }).SingleInstance();
 
         return containerBuilder.Build();
     }
--- WeatherBot/WeatherBot/Program.cs
+++ WeatherBot/WeatherBot/Program.cs
@@ -1,3 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Autofac;
+using Vostok.Logging.Abstractions;
+using WeatherBot.DI;
 
-Console.WriteLine("Hello, World!");
+namespace WeatherBot;
+
+public static class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var container = BotContainerBuilder.Build();
+
+        var log = container.Resolve<ILog>();
+
+        log.Info("Test");
+
+        // await Task.Delay(-1);
+    }
+}
