a05f0f768b8b6d9c82e5d9df5bf7dd20b4525214
Pavuuk <88192739+Pavuuk@users.noreply.github.com>
2023-01-03 17:55:52 +0500

    Add WeatherApiResponse deserializing

---
+++ WeatherBot/WeatherBot/Domain/Weather/Helpers/WeatherHelper.cs
@@ -0,0 +1,9 @@
+﻿namespace WeatherBot.Domain.Weather.Helpers;
+
+public static class WeatherHelper
+{
+    public static class CountryCode
+    {
+        public static string Russia = "ISO 3166-2:RU";
+    }
+}
---
+++ WeatherBot/WeatherBot/Domain/Weather/Models/CityCoordinates.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Cryptography;
+
+namespace WeatherBot.Domain.Weather.Models;
+
+public class CityCoordinates
+{
+    public float Lat { get; set; }
+    public float Lon { get; set; }
+    public string Name { get; set; }
+}
