1eabf0b3b5d7011f9dc49e39847dbc024d3dba22
Tr1xxter <cmango67@gmail.com>
2023-01-03 19:23:20 +0500

    Little fix

--- WeatherBot/WeatherBot/Domain/Telegram/Commands/Managers/PrivateCommandManager.cs
+++ WeatherBot/WeatherBot/Domain/Telegram/Commands/Managers/PrivateCommandManager.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Telegram.Bot.Types;
+﻿using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 using Vostok.Logging.Abstractions;
 using WeatherBot.Domain.Telegram.Clients;
--- WeatherBot/WeatherBot/Domain/Telegram/Commands/PrivateCommands/StartCommand.cs
+++ WeatherBot/WeatherBot/Domain/Telegram/Commands/PrivateCommands/StartCommand.cs
@@ -1,5 +1,4 @@
-﻿using System.Threading.Tasks;
-using Telegram.Bot.Types;
+﻿using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 using WeatherBot.Domain.Telegram.Clients;
 using WeatherBot.Domain.Telegram.Helpers;
